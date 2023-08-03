0d5158a850eeff47fc0f1eb5ab7b578f1b071244
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-02 21:13:30 -0300

    update app

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,31 +1,58 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+import api from "../api/index";
+import {Candidate, Step} from "../types/candidate";
+import {Column} from "../components/Column";
 
 import styles from "./App.module.scss";
 
+const steps: Step[] = [
+  "Entrevista inicial",
+  "Entrevista técnica",
+  "Oferta",
+  "Asignación",
+  "Rechazo",
+];
+
 function App() {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    api.candidates.list().then((data) => setCandidates(data));
+  }, []);
+
+  const addCandidate = (candidato: Candidate) => {
+    setCandidates([...candidates, candidato]);
+  };
+
+  const advance = (id: Candidate["id"], action: "avanzar" | "retroceder") => {
+    const candidateIndex = candidates.findIndex((candidate) => candidate.id === id)!;
+    const candidate = candidates[candidateIndex];
+    const indexStep = steps.indexOf(candidate.step);
+
+    const step = action === "avanzar" ? steps[indexStep + 1] : steps[indexStep - 1];
+
+    if (step) {
+      const newCandidates = [...candidates];
+
+      candidate.step = step;
+      newCandidates[candidateIndex] = candidate;
+      setCandidates(newCandidates);
+    }
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.column}>
-        <h2>Entrevista inicial</h2>
-        <p>No hay candidatos</p>
-        <button>Agregar candidato</button>
-      </div>
-      <div className={styles.column}>
-        <h2>Entrevista técnica</h2>
-        <p>No hay candidatos</p>
-      </div>
-      <div className={styles.column}>
-        <h2>Oferta</h2>
-        <p>No hay candidatos</p>
-      </div>
-      <div className={styles.column}>
-        <h2>Asignación</h2>
-        <p>No hay candidatos</p>
-      </div>
-      <div className={styles.column}>
-        <h2>Rechazo</h2>
-        <p>No hay candidatos</p>
-      </div>
+      {steps.map((step) => (
+        <Column
+          key={step}
+          addCandidate={addCandidate}
+          advance={advance}
+          candidatos={candidates.filter((candidate) => candidate.step === step)}
+          showButton={step === "Entrevista inicial"}
+          titulo={step}
+        />
+      ))}
     </main>
   );
 }
