5c44992ad201fa0815dcdfb71fecafeafc3e7da8
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-06 22:00:02 -0300

    update globals

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/ban-types */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import type {TestingLibraryMatchers} from "@testing-library/jest-dom/matchers";
 
 declare global {
   namespace jest {
-    type Matchers<R = void, T = {}> = TestingLibraryMatchers<typeof expect.stringContaining, R>;
+    interface Matchers<R = void>
+      extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
   }
 }
